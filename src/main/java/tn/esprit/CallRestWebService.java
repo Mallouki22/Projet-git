237ab7acc7d8eb77d9f6a3bb8646aa1898662167
Youssef Mallouki <youssef.mallouki@esprit.tn>
2023-11-18 16:04:45 +0100

    Third commit

--- src/main/java/tn/esprit/CallRestWebService.java
+++ src/main/java/tn/esprit/CallRestWebService.java
@@ -20,6 +20,7 @@ import org.json.JSONObject;
  */
 // First commit;
 	//Commentaire ekher
+	// Troisiéme commentaire
 public class CallRestWebService {
 
 	public static final String endpoint = "http://ip-api.com/json";
